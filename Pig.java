5f3d78a7af8abf881bb22105e708696ff2a714ea
foxy320 <sfox15@lcscourses.net>
2015-02-23 10:21:06 -0500

    created my animal
    created my animal changed my color

---
+++ Pig.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+
+public class Pig extends Turtle
+{ 
+  /**
+   * Constructor that takes a model display and adds
+   * a pig in the middle of it
+   * @param display the model display
+   */
+  public Pig(World wref)
+  {
+    super(wref);
+    Color shellColor = new Color(255, 0, 127); //requires import line at top to work (new color defined using RGB values)
+    this.setShellColor(shellColor);
+  }
+  
+  /**
+   * Constructor that takes the x and y position and the
+   * model displayer
+   * @param x the x pos
+   * @param y the y pos
+   * @param display the model display
+   */
+  public Pig(int x, int y, World wref)
+  {
+    super(x, y, wref);
+  }
+}

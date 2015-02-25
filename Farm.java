ebadbc01358ca303370aab1e2570b27ce1da67a0
foxy320 <sfox15@lcscourses.net>
2015-02-25 11:00:43 -0500

    created my farm
    increased my piggies size

---
+++ Farm.java
@@ -0,0 +1,11 @@
+public class Farm
+{
+  public static void main(String[] a)
+  {
+    World wref = new World();
+    Pig ex1 = new Pig(wref);
+    System.out.println(ex1.getWidth());
+    ex1.setWidth(ex1.getWidth() + 5);
+    System.out.println(ex1.getWidth());
+  }
+}

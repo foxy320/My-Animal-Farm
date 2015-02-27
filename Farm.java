c3d1d134a81f2bfdede474a7a2ae8b493161c1ca
sribet <srib15@lcscourses.net>
2015-02-27 10:19:01 -0500

    added animal

--- Farm.java
+++ Farm.java
@@ -4,6 +4,7 @@ public class Farm
   {
     World wref = new World();
     Pig ex1 = new Pig(wref);
+    Frog ex2 = new Frog(wref);
     System.out.println(ex1.getWidth());
     ex1.setWidth(ex1.getWidth() + 5);
     System.out.println(ex1.getWidth());

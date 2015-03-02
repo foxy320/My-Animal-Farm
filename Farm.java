18fb4b14c1f3293e33a39d4e7a13249365d7c9e4
foxy320 <sfox15@lcscourses.net>
2015-03-02 10:17:46 -0500

    Added to Farm
    attempted to give the pig directions and tried positioning it

--- Farm.java
+++ Farm.java
@@ -1,11 +1,36 @@
+import java.awt.Color;
+
 public class Farm
 {
   public static void main(String[] a)
   {
     World wref = new World();
-    Pig ex1 = new Pig(wref);
-    System.out.println(ex1.getWidth());
-    ex1.setWidth(ex1.getWidth() + 5);
-    System.out.println(ex1.getWidth());
-  }
+    Pig ex1 = new Pig(0,0, wref);
+    {
+    Color shellColor = new Color(255, 105, 180); //requires import line at top to work (new color defined using RGB values)
+    ex1.setShellColor(shellColor);
+    
+    Color bodyColor = new Color(255, 105, 180); //requires import line at top to work (new color defined using RGB values)
+    ex1.setBodyColor(bodyColor);
+    
+    ex1.setWidth(50);
+    ex1.setHeight(40);
+    
+    ex1.getDistance(10,10);
+    ex1.getDistance(25,25);}
+    
+    
+    ex1.getXPos(15);
+    ex1.getYPos(10);
+    
+    ex1.penDown(5);
+    ex1.turnLeft(20);
+    ex1. penUp(40);
+    
+    
+     
+    
+    }
+    
+}
 }
